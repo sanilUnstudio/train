cd70977ca32b4c8081f00e977a0c81445efb945b
“sanilUnstudio” <sanil@unstudio.ai>
2024-12-12 18:23:59 +0530

    remmove type check

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  ignoreBuildErrors: true,
 };
 
 export default nextConfig;
