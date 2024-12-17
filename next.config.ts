2768a9c93901fd6d8050c9ff2fb3eb6470c3a051
“sanilUnstudio” <sanil@unstudio.ai>
2024-12-17 17:10:48 +0530

    increase bodyParse size limit

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,11 @@ const nextConfig: NextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  api: {
+    bodyParser: {
+      sizeLimit: "150mb", // Increase limit as needed (e.g., 10mb, 20mb, 50mb)
+    },
+  },
 };
 
 export default nextConfig;
