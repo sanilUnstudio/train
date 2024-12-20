0a3551f90124998eed54c6083599c2689a71d355
“sanilUnstudio” <sanil@unstudio.ai>
2024-12-20 11:37:08 +0530

    fix build error

--- src/pages/components/minimalCard.tsx
+++ src/pages/components/minimalCard.tsx
@@ -23,7 +23,7 @@ export default function MinimalTrainingProgress({ items }: { items: TrainingProg
     }
     return (
         <div className="space-y-4 w-full max-w-md mx-auto">
-            {items.map((item, index) => (
+            {items?.map((item, index) => (
                 <Card key={index} className="">
                     <CardContent className="py-1 px-2 space-y-2">
                         <div className="flex justify-between items-center text-sm">
